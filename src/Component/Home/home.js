74d27a195630dd422e1102af2e6515c97750a61f
Fagley Ali Hossain <fagleyali@Fagleys-MacBook-Pro.local>
2019-09-02 11:53:26 -0500

    insert search bar

--- src/Component/Home/home.js
+++ src/Component/Home/home.js
@@ -6,6 +6,13 @@ function Home (){
     return (
         <div className='homepage-container'>
             
+            <div className='search-div'>
+            <h1 className='main-heading'>Reimagine Home</h1>
+            <h6 className='sub-heading'>We’ll help you find a place you’ll love.</h6>
+                <input type='text' placeholder='Enter an address, neighborhood, city, or Zip code' className='input-search'  />
+                <input type='text' className='input-search'  />
+            </div>
+            
         </div>
     )
 }
